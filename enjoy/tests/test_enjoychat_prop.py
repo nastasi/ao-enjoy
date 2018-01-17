d60eff1d2c211a0a38648c2a3a4e2d33a4f3f7e0
Matteo Nastasi <nastasi@alternativeoutput.it>
2018-01-17 18:18:54 +0100

    first sockjs test working test_sockjs_con

---
+++ enjoy/tests/test_enjoychat_prop.py
@@ -0,0 +1,234 @@
+from aiohttp import web
+from aiohttp.test_utils import (AioHTTPTestCase, unittest_run_loop,
+                                setup_test_loop
+                                # , teardown_test_loop
+                                )
+from .. import EnjoyChat
+import json
+
+
+class AioHTTPMultipleClientsTestCase(AioHTTPTestCase):
+    def setUp(self):
+        self.loop = setup_test_loop()
+        self.app = self.loop.run_until_complete(self.get_application())
+        super().setUp()
+        self.client2 = self.loop.run_until_complete(
+            self.get_client(self.server))
+
+    def tearDown(self):
+        self.loop.run_until_complete(self.client2.close())
+        super().tearDown()
+
+
+class EnjoyChatFakeDBTestCase(AioHTTPMultipleClientsTestCase):
+    async def get_application(self):
+        enjoy_chat = EnjoyChat(use_real_db=False)
+        app = web.Application()
+        app.on_startup.append(enjoy_chat.setup)
+
+        return app
+
+    @unittest_run_loop
+    async def test_home(self):
+        request = await self.client.request("GET", "/")
+        assert request.status == 200
+        await request.text()
+
+    @unittest_run_loop
+    async def test_login(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "user", "password": "password"})
+        assert request.status == 200
+        await request.text()
+
+    @unittest_run_loop
+    async def test_wrong_login(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "wrong_user", "password": "password"})
+        assert request.status == 401
+        await request.text()
+
+    @unittest_run_loop
+    async def test_public(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "user", "password": "password"})
+        assert request.status == 200
+        await request.text()
+        request = await self.client.request(
+            "GET", "/public")
+        assert request.status == 200
+        await request.text()
+
+    @unittest_run_loop
+    async def test_wrong_public(self):
+        request = await self.client.request(
+            "GET", "/public")
+        assert request.status == 403
+        await request.text()
+
+    @unittest_run_loop
+    async def test_protected(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "moderator", "password": "password"})
+        assert request.status == 200
+        await request.text()
+        request = await self.client.request(
+            "GET", "/protected")
+        assert request.status == 200
+        await request.text()
+
+    @unittest_run_loop
+    async def test_admin_protected(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "admin", "password": "password"})
+        assert request.status == 200
+        await request.text()
+        request = await self.client.request(
+            "GET", "/protected")
+        assert request.status == 200
+        await request.text()
+
+    @unittest_run_loop
+    async def test_wrong_protected(self):
+        request = await self.client.request(
+            "GET", "/protected")
+        assert request.status == 403
+        await request.text()
+
+    @unittest_run_loop
+    async def test_wrong2_protected(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "user", "password": "password"})
+        assert request.status == 200
+        await request.text()
+        request = await self.client.request(
+            "GET", "/protected")
+        assert request.status == 403
+        await request.text()
+
+    @unittest_run_loop
+    async def test_chat(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "user", "password": "password"})
+        assert request.status == 200
+        await request.text()
+        request = await self.client.request(
+            "GET", "/chat")
+        assert request.status == 200
+        await request.text()
+
+    @unittest_run_loop
+    async def test_logout(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "user", "password": "password"})
+        assert request.status == 200
+
+        request = await self.client.request(
+            "GET", "/logout")
+        assert request.status == 200
+
+    @unittest_run_loop
+    async def test_wrong_logout(self):
+        request = await self.client.request(
+            "GET", "/logout")
+        assert request.status == 403
+        await request.text()
+
+    @unittest_run_loop
+    async def test_wrong_sockjs_conn(self):
+        request = await self.client.request(
+            "POST", "/sockjs/666/sockjsss/xhr_streaming")
+        assert request.status == 404
+        await request.text()
+
+    @unittest_run_loop
+    async def test_sockjs_conn(self):
+        request = await self.client.request(
+            "POST", "/login", data={"login": "user", "password": "password"})
+        assert request.status == 200
+        await request.text()
+
+        request = await self.client.request(
+            "POST", "/sockjs/666/sockjsss/xhr_streaming", read_until_eof=False,
+            chunked=True)
+        self.assertEqual(request.status, 200)
+
+        request2 = await self.client.request(
+            "POST", "/sockjs/666/sockjsss/xhr_send",
+            data=json.dumps(["ROIP\nNOLLO"]))
+        self.assertEqual(request2.status, 204)
+        print("REQ2 status: [%d]" % request2.status)
+
+        print(await request.content.readchunk())
+        print(await request.content.readchunk())
+        print(await request.content.readchunk())
+        ret = await request.content.readchunk()
+
+        xx = json.loads(ret[0][1:].decode('utf-8'))
+        print(xx[0])
+        #print("prima")
+        #import pdb ; pdb.set_trace()
+        #x = json.loads(ret[0])
+        #print("dopo")
+        await self.client.session.close()
+
+    @unittest_run_loop
+    async def test_sockjs_logout(self):
+        request = await self.client.request(
+            "POST", "/login", data={
+                "login": "user", "password": "password"})
+        assert request.status == 200
+
+        request = await self.client.request(
+            "POST", "/sockjs/666/sockjsss/xhr_streaming")
+
+        request = await self.client.request(
+            "GET", "/logout")
+        print("REQ STAT: %d" % request.status)
+        assert request.status == 200
+
+
+class EnjoyChatTestCase(EnjoyChatFakeDBTestCase):
+    async def get_application(self):
+        enjoy = EnjoyChat(use_real_db=True)
+        app = web.Application()
+        app.on_startup.append(enjoy.setup)
+
+        return app
+
+
+
+
+    # the unittest_run_loop decorator can be used in tandem with
+    # the AioHTTPTestCase to simplify running
+    # tests that are asynchronous
+    # @unittest_run_loop
+    # async def test_example(self):
+    #     # pass
+    #     request = await self.clients[0].request(
+    #         "POST", "/login", data={"login": "user", "password": "password"})
+    #     self.assertEqual(request.status, 200)
+
+    #     request2 = await self.clients[1].request(
+    #         "POST", "/login", data={"login": "admin", "password": "password"})
+    #     self.assertEqual(request2.status, 200)
+
+    #     cookie = self.clients[0].session.cookie_jar.filter_cookies(
+    #         'http://127.0.0.1')
+
+    #     aiohttp_sess = json.loads(cookie['AIOHTTP_SESSION'].value)
+    #     self.assertEqual(aiohttp_sess['session']['AIOHTTP_SECURITY'], 'user')
+
+    #     cookie = self.clients[1].session.cookie_jar.filter_cookies(
+    #         'http://127.0.0.1')
+
+    #     aiohttp_sess = json.loads(cookie['AIOHTTP_SESSION'].value)
+    #     self.assertEqual(aiohttp_sess['session']['AIOHTTP_SECURITY'], 'admin')
